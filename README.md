b9f84bee43e4ce166ce7794098fb8b16dcec8711
maneeshkumarkg <21f3001443@ds.study.iitm.ac.in>
2025-08-10 14:36:27 +0530

    Update

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # project-data-analyst-agent
 
 ## App1
-![Graph](graph.png)
+![Graph](graph1.png)
+
+## App1
+![Graph](graph2.png)
--- app2.py
+++ app2.py
@@ -4,7 +4,8 @@ from langgraph.graph import StateGraph, START, END
 class PortfolioState(TypedDict):
     amount_usd: float
     total_usd: float
-    total_inr: float
+    target_currency: Literal["INR", "EUR"]
+    total: float
 
 def calc_total(state: PortfolioState) -> PortfolioState:
     """
@@ -17,22 +18,42 @@ def convert_to_inr(state: PortfolioState) -> PortfolioState:
     """
     Convert the total value to INR.
     """
-    state['total_inr'] = state['total_usd'] * 85
+    state['total'] = state['total_usd'] * 85
     return state
 
+def convert_to_eur(state: PortfolioState) -> PortfolioState:
+    """
+    Convert the total value to EUR.
+    """
+    state['total'] = state['total_usd'] * 0.9
+    return state
 
+def choose_conversion(state: PortfolioState) -> str:
+    """
+    Choose the conversion function based on the target currency.
+    """
+    return state['target_currency']
 
 builder = StateGraph(PortfolioState)
 
 builder.add_node("calc_total", calc_total)
 builder.add_node("convert_to_inr", convert_to_inr)
+builder.add_node("convert_to_eur", convert_to_eur)
 
 builder.add_edge(START, "calc_total")
-builder.add_edge("calc_total", "convert_to_inr")
+builder.add_conditional_edges(
+    "calc_total",
+    choose_conversion,
+    {
+        "INR": "convert_to_inr",
+        "EUR": "convert_to_eur"
+    }
+)
 builder.add_edge("convert_to_inr", END)
+builder.add_edge("convert_to_eur", END)
 
 graph = builder.compile()
 
 png_bytes = graph.get_graph().draw_mermaid_png()
-with open("graph1.png", "wb") as f:
+with open("graph2.png", "wb") as f:
     f.write(png_bytes)
